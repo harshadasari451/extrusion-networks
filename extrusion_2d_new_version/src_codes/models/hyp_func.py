b6416c6b762cdb520c5a8a44cf573e23708cdd60
harshadasari451 <77429662+harshadasari451@users.noreply.github.com>
2025-08-07 12:55:07 -0400

    Update hyp_func.py

--- extrusion_2d_new_version/src_codes/models/hyp_func.py
+++ extrusion_2d_new_version/src_codes/models/hyp_func.py
@@ -13,6 +13,7 @@ class Unet_blocks_hyp(nn.Module):
         self.w1 = Parameter(torch.fmod(torch.randn((self.z_dim, self.out_size*self.f_size)).cuda(),2))
         self.b1 = Parameter(torch.fmod(torch.randn((self.out_size*self.f_size)).cuda(),2))
 
+        #need to change the hyp_parameter 
         self.hyp_linear = nn.Linear(4,8).cuda()
 
         self.w2 = Parameter(torch.fmod(torch.randn(((self.z_dim + 8), self.in_size*self.z_dim)).cuda(),2))
@@ -35,35 +36,3 @@ class Unet_blocks_hyp(nn.Module):
         return kernel
 
 
-class Unet_upconv_hyp(nn.Module):
-
-    def __init__(self, f_size = 2, z_dim = 64, out_size=32, in_size=32):
-        super(Unet_upconv_hyp, self).__init__()
-        self.z_dim = z_dim
-        self.f_size = f_size
-        self.out_size = out_size
-        self.in_size = in_size
-
-        self.w1 = Parameter(torch.fmod(torch.randn((self.z_dim, self.out_size*self.f_size)).cuda(),2))
-        self.b1 = Parameter(torch.fmod(torch.randn((self.out_size*self.f_size)).cuda(),2))
-
-        self.hyp_linear = nn.Linear(4,8).cuda()
-
-        self.w2 = Parameter(torch.fmod(torch.randn(((self.z_dim+8), self.in_size*self.z_dim)).cuda(),2))
-        self.b2 = Parameter(torch.fmod(torch.randn((self.in_size*self.z_dim)).cuda(),2))
-
-
-    def forward(self, z, pde_inputs):
-
-        pde_features = self.hyp_linear(pde_inputs)
-        pde_features = torch.relu(pde_features)  # Non-linear activation after linear layer
-        z_pde_features = torch.cat((z, pde_features), dim=0)
-
-        h_in = torch.matmul(z_pde_features, self.w2) + self.b2
-        h_in = torch.relu(h_in)  # Activation heres
-        h_in = h_in.view(self.in_size, self.z_dim)
-
-        h_final = torch.matmul(h_in, self.w1) + self.b1
-        kernel = h_final.view(self.out_size, self.in_size, self.f_size)
-
-        return kernel
